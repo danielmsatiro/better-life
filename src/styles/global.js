72713b770aed8224b9ea0fbbd34ce1e6b9be0dff
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2021-12-08 20:21:52 -0300

    style: create root-colors

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,8 +8,23 @@ body, div, h1{
     outline: 0;
 }
 
+li {
+    list-style: none;
+}
+
 :root{
-    
+    --light-purple: #A5A6F6;
+    --purple: #5D5FEF;
+    --dark-purple: #1F161E;
+    --green: #55B79D;
+    --light-green: #ACE5E3;
+    --gray: #474747;
+    --light-gray: #C4C4C4;
+    --pink: #FA9E9F;
+    --success: #27A300;
+    --error: #FF0000;
+    --error-light: #FFEBEB;
+    --yellow: #FBDC8E;
 }
 
 body{
@@ -17,5 +32,4 @@ body{
     color: var();
     background-color: var();
 }
-
 `;
