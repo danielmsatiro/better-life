a7e12e012271829a240ba3b68203f07bd2f44b5f
Guilherme Couto <gcoutos95@gmail.com>
2021-12-09 08:58:45 -0300

    Merging whith develop

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import Providers from "./providers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
-=======
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Providers>
+        <App />
+      </Providers>
     </BrowserRouter>
->>>>>>> develop
   </React.StrictMode>,
   document.getElementById("root")
 );
