f777f847d0b57d7e1d725ac1660c9174e19ce8ad
Pedro Basilio <pedronobasi@gmail.com>
2021-12-10 08:52:30 -0300

    feat: graph

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -1,24 +1,25 @@
-import {Chart} from "react-google-charts"
+import {Chart} from "react-google-charts";
 
 function Graph(){
     return(
         <div>
             <Chart
-                width={'200px'}
-                height={'200px'}
+                width={'300px'}
+                height={'300px'}
                 chartType="PieChart"
                 loader={<div>Loading Chart</div>}
                 data={[
                     ['Task', 'Hours per Day'],
-                    ['Saúde', 11],
-                    ['Trabalho', 2],
-                    ['Família', 2],
-                    ['Cultura', 2],
+                    ['Work', 11],
+                    ['Eat', 2],
+                    ['Commute', 2],
+                    ['Watch TV', 2],
+                    ['Sleep', 7],
                 ]}
                 options={{
-                    title: '',
+                    title: 'My Daily Activities',
                     // Just add this option
-                    pieHole: 0.2,
+                    pieHole: 0.5,
                 }}
                 rootProps={{ 'data-testid': '3' }}
             />
