c3e107a89ce642fd33ca5d53c70e4faa5911c873
HardBoris <bgaibor03@gmail.com>
2021-12-13 17:01:59 -0400

    feat: maker e editor ready

--- src/pages/Prueba/index.jsx
+++ src/pages/Prueba/index.jsx
@@ -8,12 +8,15 @@ export const Prueba = () => {
   const handleTechModal = () => {
     setTechModal(!techModal);
   };
-
+  const formId = "conocido";
   return (
     <Layout>
       <button onClick={() => setTechModal(true)}>Abrir modal de prueba</button>
       <Modal isOpen={techModal} setIsOpen={handleTechModal}>
-        <HabitMaker closeFunction={handleTechModal}></HabitMaker>
+        <HabitMaker
+          closeFunction={handleTechModal}
+          identity={formId}
+        ></HabitMaker>
       </Modal>
     </Layout>
   );
