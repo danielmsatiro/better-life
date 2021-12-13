811fa8026479a375257fe86009af89085b1be1b6
Pedro Basilio <pedronobasi@gmail.com>
2021-12-13 15:52:49 -0300

    fix: render graph

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -13,37 +13,33 @@ function HabitsGraph(){
         return myHabits.filter((item) => item.category === category);
     }
 
-    // const DomesticosCategoryInitial    = findCategory("Domésticos")
-    // const EspiritualCategoryInitial    = findCategory("Espiritual")
-    // const IntelectualCategoryInitial   = findCategory("Intelectual")
-    // const LazerCategoryInitial         = findCategory("Lazer")
-    // const ProfissionalCategoryInitial  = findCategory("Profissional")
+    const DomesticosCategoryInitial    = findCategory("Domésticos")
+    const EspiritualCategoryInitial    = findCategory("Espiritual")
+    const IntelectualCategoryInitial   = findCategory("Intelectual")
+    const LazerCategoryInitial         = findCategory("Lazer")
+    const ProfissionalCategoryInitial  = findCategory("Profissional")
     const SaudeCategoryInitial         = findCategory("Saúde")
 
-    // const [DomesticosCategory, setDomésticosCategory]      = useState(DomesticosCategoryInitial)
-    // const [EspiritualCategory, setEspiritualCategory]      = useState(EspiritualCategoryInitial)
-    // const [IntelectualCategory , setIntelectualCategory]   = useState(IntelectualCategoryInitial)
-    // const [LazerCategory   , setLazerCategory]             = useState(LazerCategoryInitial)
-    // const [ProfissionalCategory, setProfissionalCategory]  = useState(ProfissionalCategoryInitial)
-    const [SaudeCategory, setSaudeCategory]                = useState(SaudeCategoryInitial)
+    const [DomesticosCategory, setDomésticosCategory]      = useState([])
+    const [EspiritualCategory, setEspiritualCategory]      = useState([])
+    const [IntelectualCategory , setIntelectualCategory]   = useState([])
+    const [LazerCategory   , setLazerCategory]             = useState([])
+    const [ProfissionalCategory, setProfissionalCategory]  = useState([])
+    const [SaudeCategory, setSaudeCategory]                = useState([])
 
     const updateGraph = () => {
-    //     setDomésticosCategory(findCategory("Domésticos"));
-    //     setEspiritualCategory(findCategory("Espiritual"));
-    //     setIntelectualCategory(findCategory("Intelectual"));
-    //     setLazerCategory(findCategory("Lazer"));
-    //     setProfissionalCategory(findCategory("Profissional"));
+        setDomésticosCategory(findCategory("Domésticos"));
+        setEspiritualCategory(findCategory("Espiritual"));
+        setIntelectualCategory(findCategory("Intelectual"));
+        setLazerCategory(findCategory("Lazer"));
+        setProfissionalCategory(findCategory("Profissional"));
         setSaudeCategory(findCategory("Saúde"));
-        // console.log("opa")
     }
-    // console.log(SaudeCategory)
 
     useEffect(() => {
         updateGraph()
         setTrack(true)
       }, []);
-      console.log(track)
-      console.log("att")
 
 
     return(
@@ -65,7 +61,7 @@ function HabitsGraph(){
                         pieHole: 0.0,
                     }}
                     rootProps={{ 'data-testid': '3' }}
-                /> 
+                />
                 :
                 <Chart
                 width={'300px'}
@@ -74,12 +70,12 @@ function HabitsGraph(){
                 loader={<div>Loading Chart</div>}
                 data={[
                     ['Task', 'Hours per Day'],
-                    // ['Domésticos',      DomesticosCategoryInitial.length],
-                    // ['Espiritual',      EspiritualCategoryInitial.length],
-                    // ['Intelectual',     IntelectualCategoryInitial.length],
-                    // ['Lazer',           LazerCategoryInitial.length],
-                    // ['Profissional',    ProfissionalCategoryInitial.length],
-                    ['Saúde',              !setTrack ? SaudeCategory.length : SaudeCategoryInitial.length],
+                    ['Domésticos',      !track ? DomesticosCategory.length   : DomesticosCategoryInitial.length],
+                    ['Espiritual',      !track ? EspiritualCategory.length   : EspiritualCategoryInitial.length],
+                    ['Intelectual',     !track ? IntelectualCategory.length  : IntelectualCategoryInitial.length],
+                    ['Lazer',           !track ? LazerCategory.length        : LazerCategoryInitial.length],
+                    ['Profissional',    !track ? ProfissionalCategory.length : ProfissionalCategoryInitial.length],
+                    ['Saúde',           !track ? SaudeCategory.length        : SaudeCategoryInitial.length],
                 ]}
                 options={{
                     width: '300px',
