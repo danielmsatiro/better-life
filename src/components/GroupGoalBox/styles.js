8cf91c3902c2c68d550d5ee087eea03d703d99a3
Pedro Basilio <pedronobasi@gmail.com>
2021-12-14 19:08:10 -0300

    feat: card goal| box goals

---
+++ src/components/GroupGoalBox/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const GoalBoxContainer = styled.section`
+    background: var(--green);
+    width: 270px;
+    padding: 10px;
+    border-radius: 10px;
+
+    .Goal_box_container_title{
+        color: white;
+        margin: 0 5px;
+        font-weight: 400 ;
+    }
+
+    .create_goal{
+        position: relative;
+        float: right;
+        right: 15px;
+        top: 18px;
+        cursor: pointer;
+    }
+
+    .create_goal_icon{
+        color: white;
+        width: 30px;
+        height: 30px;
+    }
+`
--- src/pages/Groups/index.jsx
+++ src/pages/Groups/index.jsx
@@ -1,10 +1,12 @@
 // import { Container } from './styles';
 import GoalBoxSide from "../../components/GoalBoxSide";
 import { ListMiniCards } from "../../components/ListMiniCards";
+import GroupGoalBox from "../../components/GroupGoalBox";
 
 const Groups = () => {
   return (
     <div>
+      <GroupGoalBox/>
       <GoalBoxSide/>
       <ListMiniCards />
     </div>
