67fa515cb3322c7254d2b6d13706c0dd3878ea7a
Mauro Donizeti de Oliveira Junior <maurodonizetioliveira@gmail.com>
2021-12-14 15:22:12 -0300

    fix:requisitions interactions fixed

--- src/components/HabitsCards/index.js
+++ src/components/HabitsCards/index.js
@@ -1,8 +1,14 @@
 import { Container } from "./styles";
 import { Content } from "./styles";
-import { MdDoneOutline } from "react-icons/md";
+import { MdDoneOutline, MdOutlineDoNotDisturb } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { MdClose } from "react-icons/md";
+import { HabitMaker } from "../HabitMaker";
+import { HabitEditor } from "../HabitEditor";
+import Modal from "../Modal";
+import { useState } from "react";
+import { useMyHabits } from "../../providers/myHabits";
+import ConfModal from "../ConfModal";
 
 const myHabits = [
   {
@@ -68,30 +74,62 @@ const myHabits = [
 ];
 
 const HabitsCards = () => {
+  const { myHabits, deleteHabit } = useMyHabits();
+  console.log(myHabits);
+  const [removeModal, setRemoveModal] = useState(false);
+  const [techModal, setTechModal] = useState(false);
+  const handleTechModal = () => {
+    setTechModal(!techModal);
+  };
+  const [id, setID] = useState();
+
+  const registerid = (id) => {
+    setRemoveModal(true);
+    setID(id);
+    console.log(id);
+  };
+
+  const formId = "conocido";
   return (
     <Container>
       <div>
         <h3>Hábitos</h3>
-        <button>+</button>
+        <button onClick={() => setTechModal(true)}>+</button>
+        <Modal isOpen={techModal} setIsOpen={handleTechModal}>
+          <HabitMaker
+            closeFunction={handleTechModal}
+            identity={formId}
+          ></HabitMaker>
+        </Modal>
       </div>
-      <table>
-        <Content>
-          {myHabits.map((habit) => (
-            <tr key={habit.id}>
-              <td id="title">{habit.title}</td>
-              <td>
-                <MdDoneOutline />
-              </td>
-              <td>
-                <MdEdit />
-              </td>
-              <td>
-                <MdClose />
-              </td>
-            </tr>
-          ))}
-        </Content>
-      </table>
+      <Content>
+        {myHabits.map((habit) => (
+          <tr id={habit.id}>
+            <td title="title">{habit.title}</td>
+            <td>
+              {habit.achieved ? <MdDoneOutline /> : <MdOutlineDoNotDisturb />}
+            </td>
+            <td>
+              <MdEdit onClick={() => setTechModal(true)} />
+              <Modal isOpen={techModal} setIsOpen={handleTechModal}>
+                <HabitEditor closeFunction={handleTechModal}></HabitEditor>
+              </Modal>
+            </td>
+            <td>
+              <MdClose onClick={() => registerid(habit.id)} />
+              <ConfModal
+                action={() => {
+                  deleteHabit(id);
+                  setRemoveModal(false);
+                }}
+                isOpen={removeModal}
+                setIsOpen={setRemoveModal}
+                text={"deseja remover este hábito?"}
+              />
+            </td>
+          </tr>
+        ))}
+      </Content>
     </Container>
   );
 };
