64d509cffddc656d1b06b9df095e67a8add1af00
Guilherme Couto <gcoutos95@gmail.com>
2021-12-16 10:01:04 -0300

    fix: Refactoring the header from login style.

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 const Container = styled.div`
   font-family: "Pacifico", serif;
   font-size: 1.3rem;
-  color: var(--light-purple);
+  color: var(--white);
+
+  @media (min-width: 769px) {
+    color: var(--light-purple);
+  }
 `;
 
 export default Container;
